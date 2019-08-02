8f7de86d9706b2a771d26ae51b5945eaabf1480a
annlinros <annlinros@gmail.com>
2019-08-02 13:06:30 +0530

    Update Library.js

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
+import { MovieContext } from "../Context";
 
-export default function Library({movieItem}) {
-  const { poster,year, title, imdbRating, plot } = movieItem;
-  return (
-    <div className="movie-item-container" style={{ color: "black" }}>
-      <div className="image-container">
-        <div
-          className="bg-image"
-          style={{ backgroundImage: `url(${poster})` }}
-        />
-      </div>
-      <div className="movie-info">
+export default function Library() {
+  const { movieLibrary } = useContext(MovieContext);
+
+  return movieLibrary.map(movie => {
+    const { poster, year, title, imdbRating, plot } = movie;
+    return (
+      <div className="movie-item-container" style={{ color: "black" }}>
+        <div className="image-container">
+          <div
+            className="bg-image"
+            style={{ backgroundImage: `url(${poster})` }}
+          />
+        </div>
+        <div className="movie-info">
           <h1>Title :{title}</h1>
           <small>Year: {year}</small>
           <h4>Rating: {imdbRating} / 10</h4>
           <p>{plot}</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  });
 }
