9ea22a0e930b1b5a8cbfbcfc5785707934406e81
annlinros <annlinros@gmail.com>
2019-08-02 13:03:39 +0530

    updated with useContext()

--- src/components/Movie/MovieSearch.js
+++ src/components/Movie/MovieSearch.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { MovieContext } from "../../Context";
 import Movie from "./Movie";
 import "./Movie.css";
 
-export default function MovieSearch({movieIDs,movieFound,handleSearch,getQuery,libraryItem}) {
+export default function MovieSearch() {
+  const {
+    movieIDs,
+    movieFound,
+    query,
+    getMovies,
+    getQuery,
+    handleSearch
+  } = useContext(MovieContext);
+
+  useEffect(() => {
+    getMovies();
+  }, [query]);
+
   return (
     <div>
       <div className="searchBar">
@@ -12,20 +26,19 @@ export default function MovieSearch({movieIDs,movieFound,handleSearch,getQuery,l
           placeholder="Type in a movie name.."
           onChange={handleSearch}
         />
-        <button
-          className="searchBtn"
-          type="submit"
-          onClick={getQuery}
-        >
+        <button className="searchBtn" type="submit" onClick={getQuery}>
           GO!
         </button>
       </div>
       {movieFound ? (
-        movieIDs.map(id => (
-          <Movie id={id} key={id} libraryItem={libraryItem} />
-        ))
+        movieIDs.map(id => <Movie id={id} key={id} />)
       ) : (
-        <h1 style={{ textAlign: "center", color: "black" }}>
+        <h1
+          style={{
+            textAlign: "center",
+            color: "black"
+          }}
+        >
           Movie not found,Search again!
         </h1>
       )}
