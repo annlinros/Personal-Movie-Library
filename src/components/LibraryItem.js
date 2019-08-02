b79af69e0d28041cda4c62c8939b1c3041afbb53
annlinros <annlinros@gmail.com>
2019-08-02 13:56:10 +0530

    Create LibraryItem.js

---
+++ src/components/LibraryItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function LibraryItem({movie}) {
+	
+  const { poster, year, title, imdbRating, plot } = movie;
+
+  return (
+    <div className="movie-item-container" style={{ color: "black" }}>
+      <div className="image-container">
+        <div
+          className="bg-image"
+          style={{ backgroundImage: `url(${poster})` }}
+        />
+      </div>
+      <div className="movie-info">
+        <h1>Title :{title}</h1>
+        <small>Year: {year}</small>
+        <h4>Rating: {imdbRating} / 10</h4>
+        <p>{plot}</p>
+      </div>
+    </div>
+  );
+}
