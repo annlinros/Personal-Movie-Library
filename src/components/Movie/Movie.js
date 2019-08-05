256edbd7b10a0c1c1c5023620d92c8f7fc07f017
annlinros <annlinros@gmail.com>
2019-08-05 20:10:05 +0530

    Update Movie.js

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,40 +1,37 @@
-import React, { useContext,useEffect,useState} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { MovieContext } from "../../Context";
 import MovieSearch from "./MovieSearch";
 import MovieItem from "./MovieItem";
 import "./Movie.css";
 
 export default function Movie() {
-
   // getting url from the MovieContext
   const { url } = useContext(MovieContext);
 
   // State variables
   const [movieIDs, setMovieIDs] = useState([]);
   const [movieFound, setMovieFound] = useState(true);
-
- //  API calls to the OMDB API when the url changes, and set the movieIDs array.
+  //  API calls to the OMDB API when the url changes, and set the movieIDs array.
   useEffect(() => {
     const getMovies = async () => {
-    try {
-      const apiResponse = await fetch(url);
-      const data = await apiResponse.json();
-      console.log(data);
-
-      if (data.Response === "True") {
-        setMovieIDs(data.Search.map(movie => movie.imdbID));
-        setMovieFound(true);
-      } else {
-        setMovieFound(false);
+      try {
+        const apiResponse = await fetch(url);
+        const data = await apiResponse.json();
+        console.log(data);
+        if (data.Response === "True") {
+          setMovieIDs(data.Search.map(movie => movie.imdbID));
+          setMovieFound(true);
+        } else {
+          setMovieFound(false);
+        }
+      } catch (error) {
+        console.log(error);
       }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    };
     getMovies();
   }, [url]);
 
-// maps over the movieIDs array to render MovieItem for each ID.
+  // maps over the movieIDs array to render MovieItem for each ID.
   return (
     <React.Fragment>
       <MovieSearch />
@@ -47,7 +44,7 @@ export default function Movie() {
             color: "black"
           }}
         >
-       Not Found!
+          Not Found!
         </h1>
       )}
     </React.Fragment>
