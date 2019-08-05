5e9ed8af925f3c695574fc5ff8c80b3f89b84d8f
annlinros <annlinros@gmail.com>
2019-08-05 18:10:51 +0530

    removed initial url

--- src/Context.js
+++ src/Context.js
@@ -3,11 +3,11 @@ import React, { useState, createContext } from "react";
 export const MovieContext = createContext();
 
 export const MovieProvider = props => {
-  
+
   const [search, setSearch] = useState("");
   const [movieLibrary, setMovieLibrary] = useState([]);
   const [url, setUrl] = useState(
-    `http://www.omdbapi.com/?apikey=5093026f&s=Flipped`
+    // `http://www.omdbapi.com/?apikey=5093026f&s=Flipped`
   );
 
   const handleSearch = e => {
