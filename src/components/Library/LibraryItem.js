e3cb9b75140656b30a86514e85fc447b4d8aeb51
annlinros <annlinros@gmail.com>
2019-08-12 22:03:45 +0530

    Remove btn

--- src/components/Library/LibraryItem.js
+++ src/components/Library/LibraryItem.js
@@ -1,27 +1,31 @@
-import React from "react";
-import {Col, Card, Button } from "react-bootstrap";
+import React, { useContext } from "react";
+import { MovieContext } from "../../Context";
+import { Col, Card, Button } from "react-bootstrap";
 
 export default function LibraryItem({ movie }) {
+  const { movieLibrary, setMovieLibrary} = useContext(MovieContext);
+  
+  // Removes movie from library.
+  const removeFromLibrary = id => {
+    const tempLibrary = [...movieLibrary];
+    const newLibrary = tempLibrary.filter(item => item.id !== id);
+    setMovieLibrary(newLibrary);
+  };
+
   // renders out each movie item in library.
-  const { poster, year, title, imdbRating } = movie;
+  const { id, poster, title} = movie;
 
   return (
-    <Col xs={12} md={6} lg={4}>
-    <Card
-      className="mx-auto p-2 mb-1"
-      border="secondary"
-      style={{ height: "100vh", color: "black" }}
-    >
-      <Card.Img style={{ height: "12rem" }} src={poster} />
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Subtitle className="mb-2">
-          Rating : {imdbRating}
-        </Card.Subtitle>{" "}
-        <Card.Subtitle>Year: {year}</Card.Subtitle>
-        <Button variant="success">Mark Seen</Button>
-      </Card.Body>
-    </Card>
+    <Col xs={10} sm={6} md={4} lg={3} className="py-2">
+      <Card className="mx-auto p-2" border="secondary">
+        <Card.Img style={{ height: "12rem" }} src={poster} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Button variant="warning" onClick={() => removeFromLibrary(id)}>
+            Remove
+          </Button>
+        </Card.Body>
+      </Card>
     </Col>
   );
 }
